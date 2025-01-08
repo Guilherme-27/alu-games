0ae51891e713f4c2dc70f2fb9bc89dac44e16465
Guilherme Gabriel <guilhermegabriel.mf@gmail.com>
2025-01-08 15:15:25 -0300

    revisado

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
-function alterarStatus(numeroJogo) {
-    botoes = document.getElementsByClassName('dashboard__item__button');
-    textoBotao = botoes[numeroJogo-1].innerHTML;
-    if (textoBotao == 'Alugar') {
-        botoes[numeroJogo-1].innerHTML = 'Devolver';
-        botoes[numeroJogo-1].classList.add('dashboard__item__button--return');
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (botao.classList.contains('dashboard__item__button--return')) {
+        botao.classList.remove('dashboard__item__button--return');
+        imagem.classList.remove('dashboard__item__img--rented');
+        //innerHTML também pode ser substituído por .textContent
+        botao.innerHTML = 'Alugar';
     } else {
-        botoes[numeroJogo-1].innerHTML = 'Alugar';
-        botoes[numeroJogo-1].classList.remove('dashboard__item__button--return');
+        botao.classList.add('dashboard__item__button--return');
+        imagem.classList.add('dashboard__item__img--rented');
+        //innerHTML também pode ser substituído por .textContent
+        botao.innerHTML = 'Devolver';
     }
 }

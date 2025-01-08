5314156d0ad27dc500c7a9a620e9c8b6afbcfd0b
Guilherme Gabriel <guilhermegabriel.mf@gmail.com>
2025-01-08 15:20:18 -0300

    confirmação

--- js/app.js
+++ js/app.js
@@ -2,16 +2,17 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-
-    if (botao.classList.contains('dashboard__item__button--return')) {
-        botao.classList.remove('dashboard__item__button--return');
-        imagem.classList.remove('dashboard__item__img--rented');
-        //innerHTML também pode ser substituído por .textContent
-        botao.innerHTML = 'Alugar';
-    } else {
-        botao.classList.add('dashboard__item__button--return');
-        imagem.classList.add('dashboard__item__img--rented');
-        //innerHTML também pode ser substituído por .textContent
-        botao.innerHTML = 'Devolver';
+    if (confirm('Você tem certeza?') == true) {
+        if (botao.classList.contains('dashboard__item__button--return')) {
+            botao.classList.remove('dashboard__item__button--return');
+            imagem.classList.remove('dashboard__item__img--rented');
+            //innerHTML também pode ser substituído por .textContent
+            botao.innerHTML = 'Alugar';
+        } else {
+            botao.classList.add('dashboard__item__button--return');
+            imagem.classList.add('dashboard__item__img--rented');
+            //innerHTML também pode ser substituído por .textContent
+            botao.innerHTML = 'Devolver';
+        }
     }
 }
